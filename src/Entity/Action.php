4696646ee9c99ddb5afdd2e414b14b2cbe48326b
JAPHET NAMDEGONE <japhet.namdegone@yperia.fr>
2021-01-22 09:26:55 +0100

    modif action

--- src/Entity/Action.php
+++ src/Entity/Action.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ActionRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 /**
  * @ORM\Entity(repositoryClass=ActionRepository::class)
