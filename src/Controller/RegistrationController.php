a39887f722d150a1e4daed5f50fc1b95f71fefda
Alexis Brohan <alexis.brohan@imie.fr>
2021-01-29 17:14:27 +0100

    Fix pseudo

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -27,15 +27,15 @@ class RegistrationController extends AbstractController
     {
         
         $content = json_decode($request->getContent(), true);
-        if(!isset($content['email'], $content['pseudo'], $content['password'])) {
+        if(!isset($content['email'], $content['password'])) {
             return new Response('', 400);
         }
         $user = new User();
         $user->setEmail($content['email']);
-        $user->setPseudo($content['pseudo']);
+        $user->setPseudo(isset($content['pseudo']) ? $content['pseudo'] : '');
         $user->setPassword($this->passwordEncoder->encodePassword($user, $content['password']));
         $em = $this->getDoctrine()->getManager();
-        if($em->getRepository(User::class)->findOneBy(['email' => $user->getEmail()]) || $em->getRepository(User::class)->findOneBy(['pseudo' => $user->getPseudo()])) {
+        if($em->getRepository(User::class)->findOneBy(['email' => $user->getEmail()])) {
             return new Response('', 409);
         }
         $em->persist($user);
